b4fce24603b39d8c8a98ec8a3a962b2d08c5cdcf
Facundo Nahuel Casal <fcasal92@gmail.com>
2022-05-07 16:09:09 -0300

    agregado de pagina en mantenimiento

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-//temporizador
+// //temporizador
 
-const getRemainTime = (deadline) => {
-    let now = new Date(),
+// const getRemainTime = (deadline) => {
+//     let now = new Date(),
 
-    remainTime = (new Date(deadline) - now + 1000) / 1000,
+//     remainTime = (new Date(deadline) - now + 1000) / 1000,
 
-    remainseconds = ( `0` + Math.floor( remainTime % 60)).slice(-2),
+//     remainseconds = ( `0` + Math.floor( remainTime % 60)).slice(-2),
 
-    remainMinutos = ( `0` + Math.floor( remainTime /60 % 60)).slice(-2),
+//     remainMinutos = ( `0` + Math.floor( remainTime /60 % 60)).slice(-2),
 
-    remainHours =  ( `0` + Math.floor( remainTime /3600 % 24)).slice(-2),
+//     remainHours =  ( `0` + Math.floor( remainTime /3600 % 24)).slice(-2),
 
-    remainDays = Math.floor( remainTime / (3600 * 24))
+//     remainDays = Math.floor( remainTime / (3600 * 24))
 
-    return{
-        remainTime,
-        remainseconds,
-        remainMinutos,
-        remainHours,
-        remainDays
-    }
-}
+//     return{
+//         remainTime,
+//         remainseconds,
+//         remainMinutos,
+//         remainHours,
+//         remainDays
+//     }
+// }
 
-const countdown = (deadline , elem , finalMessage) => {
+// const countdown = (deadline , elem , finalMessage) => {
 
-    let el = document.getElementById(elem);
+//     let el = document.getElementById(elem);
 
-    const timeUpdate = setInterval( () => {
+//     const timeUpdate = setInterval( () => {
         
-        let t = getRemainTime(deadline);
+//         let t = getRemainTime(deadline);
 
-        // el.innerHTML = `<p class="cuando__contenido__texto" id="temporizador">${t.remainDays}</p>`
-        el.innerHTML = `${t.remainDays}  Dias  ${t.remainHours} Hs  ${t.remainMinutos} M  ${t.remainseconds} S`
+//         // el.innerHTML = `<p class="cuando__contenido__texto" id="temporizador">${t.remainDays}</p>`
+//         el.innerHTML = `${t.remainDays}  Dias  ${t.remainHours} Hs  ${t.remainMinutos} M  ${t.remainseconds} S`
         
-        if (t.remainTime <= 1){
-            clearInterval(timeUpdate)
-            el.innerHTML = finalMessage
-        }
-    }, 1000)
-}
+//         if (t.remainTime <= 1){
+//             clearInterval(timeUpdate)
+//             el.innerHTML = finalMessage
+//         }
+//     }, 1000)
+// }
 
-countdown(`Oct 07 2022 00:00:00 GMT-0300` ,`temporizador`,`Es Hoy`)
+// countdown(`Oct 07 2022 00:00:00 GMT-0300` ,`temporizador`,`Es Hoy`)
